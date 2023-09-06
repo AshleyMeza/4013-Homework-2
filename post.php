b4fb5ebc948048595ca5c737ceb262a1ffa4d332
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-09-06 14:11:03 -0500

    Update post.php

--- post.php
+++ post.php
@@ -3,7 +3,7 @@
   include "view-header.php";
 ?>
     <h1>post</h1>
-<form method="psot" action="post-result.php">
+<form method="post" action="post-result.php">
   <input type ="text" name="my-name">
   <input type ="submit" value ="Post data">
 </form>
