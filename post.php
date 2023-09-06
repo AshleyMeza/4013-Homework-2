5d02af3f86d87e0bda0c639ffdbcf1eb7ca2fa7f
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-09-06 14:00:36 -0500

    Create post.php

---
+++ post.php
@@ -0,0 +1,12 @@
+<?php
+  $pageTitle ="Post";
+  include "view-header.php";
+?>
+    <h1>post</h1>
+<form method="psot" action="post-result.php">
+  <input type ="text" name="my-name">
+  <input type ="submit" value ="Post data">
+</form>
+<?php
+  include "view-footer.php";
+?>
